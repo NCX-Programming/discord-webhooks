7568b7f8d169e9365e0abcf437d7c3a079574eeb
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-10-16 12:38:08 -0500

    Create send-ghactions.sh
    this is nowhere near something useable, lol

---
+++ send-ghactions.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+if [ -z "$2" ]; then
+  echo -e "WARNING!!\nYou need to pass the WEBHOOK_URL environment variable as the second argument to this script.\nFor details & guide, visit: https://github.com/DS-Homebrew/discord-webhooks" && exit
+fi
+
+echo -e "[Webhook]: Sending webhook to Discord...\\n";
+
+case $1 in
+  "success" )
+    EMBED_COLOR=3066993
+    STATUS_MESSAGE="Passed"
+    AVATAR="https://raw.githubusercontent.com/Universal-Team/discord-webhooks/master/github-logo.png"
+    ;;
+
+  "failure" )
+    EMBED_COLOR=15158332
+    STATUS_MESSAGE="Failed"
+    AVATAR="https://raw.githubusercontent.com/Universal-Team/discord-webhooks/master/github-logo.png"
+    ;;
+
+  * )
+    EMBED_COLOR=0
+    STATUS_MESSAGE="Status Unknown"
+    AVATAR="https://raw.githubusercontent.com/Universal-Team/discord-webhooks/master/github-logo.png"
+    ;;
+esac
+
+AUTHOR_NAME="$(git log -1 "$BUILD_SOURCEVERSION" --pretty="%aN")"
+COMMITTER_NAME="$(git log -1 "$BUILD_SOURCEVERSION" --pretty="%cN")"
+COMMIT_SUBJECT="$(git log -1 "$BUILD_SOURCEVERSION" --pretty="%s")"
+COMMIT_MESSAGE="$(git log -1 "$BUILD_SOURCEVERSION" --pretty="%b")"
+SOURCEBRANCH=${BUILD_SOURCEBRANCH##*/}
+
+if [ "$AUTHOR_NAME" == "$COMMITTER_NAME" ]; then
+  CREDITS="$github.actor authored & committed"
+else
+  CREDITS="$github.actor authored & $COMMITTER_NAME committed"
+fi
+
+TIMESTAMP=$(date --utc +%FT%TZ)
+if [ $IMAGE = "" ]; then
+  WEBHOOK_DATA='{
+    "username": "",
+    "avatar_url": "https://raw.githubusercontent.com/Universal-Team/discord-webhooks/master/github-logo.png",
+    "embeds": [ {
+      "color": '$EMBED_COLOR',
+      "author": {
+        "name": "Build '"v$CURRENT_DATE"' '"$STATUS_MESSAGE"' - '"$REPOSITORY_NAME"'",
+        "url": "'"https://example.org"'",
+        "icon_url": "'$AVATAR'"
+      },
+      "title": "'"$COMMIT_SUBJECT"'",
+      "url": "'"$URL"'",
+      "description": "'"${COMMIT_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
+      "fields": [
+        {
+          "name": "Commit",
+          "value": "'"[\`${BUILD_SOURCEVERSION:0:7}\`](https://github.com/$REPOSITORY_NAME/commit/$BUILD_SOURCEVERSION)"'",
+          "inline": true
+        },
+        {
+          "name": "Branch",
+          "value": "'"[\`$SOURCEBRANCH\`](https://github.com/$REPOSITORY_NAME/tree/$SOURCEBRANCH)"'",
+          "inline": true
+        },
+        {
+          "name": "Release",
+          "value": "'"[\`v$CURRENT_DATE\`](https://github.com/Universal-Team/extras/releases/tag/v$CURRENT_DATE)"'",
+          "inline": true
+        }
+      ],
+      "timestamp": "'"$TIMESTAMP"'"
+    } ]
+  }'
+else
+  WEBHOOK_DATA='{
+    "username": "",
+    "avatar_url": "https://raw.githubusercontent.com/Universal-Team/discord-webhooks/master/github-logo.png",
+    "embeds": [ {
+      "color": '$EMBED_COLOR',
+      "author": {
+        "name": "Build '"github.sha"' '"$STATUS_MESSAGE"' - '"$REPOSITORY_NAME"'",
+        "url": "'"https://dev.azure.com/universal-team/Builds/_build/results?buildId=$BUILD_BUILDID"'",
+        "icon_url": "'$AVATAR'"
+      },
+      "title": "'"$COMMIT_SUBJECT"'",
+      "url": "'"$URL"'",
+      "description": "'"${COMMIT_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
+      "fields": [
+        {
+          "name": "Commit",
+          "value": "'"[\`${BUILD_SOURCEVERSION:0:7}\`](https://github.com/$REPOSITORY_NAME/commit/$BUILD_SOURCEVERSION)"'",
+          "inline": true
+        },
+        {
+          "name": "Branch",
+          "value": "'"[\`$SOURCEBRANCH\`](https://github.com/$REPOSITORY_NAME/tree/$SOURCEBRANCH)"'",
+          "inline": true
+        },
+        {
+          "name": "Release",
+          "value": "'"[\`v$CURRENT_DATE\`](https://github.com/Universal-Team/extras/releases/tag/v$CURRENT_DATE)"'",
+          "inline": false
+        }
+      ],
+      "image": {
+        "url": "'"$IMAGE"'"
+      },
+      "timestamp": "'"$TIMESTAMP"'"
+    } ]
+  }'
+fi
+
+(curl --fail --progress-bar -A "Azure-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "$WEBHOOK_DATA" "$2" \
+  && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
